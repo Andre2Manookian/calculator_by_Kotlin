5f1fac9708f0e2b0485b552d9df1a8e1908f072b
Andre Manookian <99414204+Andre2Manookian@users.noreply.github.com>
2022-10-19 21:03:32 +0400

    Add files via upload

---
+++ Main.kt
@@ -0,0 +1,44 @@
+fun main() {
+    var number1 = 0
+    var number2 = 0
+    var operation: Char = '+'
+
+
+    println("please add you1 ")
+    number1 = readLine()!!.toInt()
+
+    println("please add your next")
+    number2 = readLine()!!.toInt()
+
+    println("please add your operation")
+    operation = readLine()!!.single()
+
+when(operation){
+    '+' -> add(number1,number2)
+        '-' -> add2(number1 , number2)
+
+
+
+}
+
+
+
+}
+
+
+fun add(num1: Int, num2: Int) {
+    println("$num1+$num2=${num1 + num2}")
+
+}
+
+fun add2(num1: Int, num2: Int) {
+    println("$num1-$num2=${num1 - num2}")
+}
+
+fun add3(num1: Int, num2: Int) {
+    println("$num1*$num2=${num1 * num2}")
+}
+
+fun add4(num1: Int, num2: Int) {
+    println("$num1/$num2=${num1 / num2}")
+}
